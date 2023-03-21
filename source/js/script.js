71c5339b6c129fbebcad37b5f1f521817ee92c4c
derekvlad <bravis96166@gmail.com>
2023-03-21 03:50:53 +0300

    add scroll to blocks

--- source/js/script.js
+++ source/js/script.js
@@ -4,22 +4,30 @@ const headerNav = document.querySelector('.header__nav');
 const linkItem = document.querySelectorAll('.header__menu-item');
 
 
-menuBtn.addEventListener('click', () =>{
-    if (headerNav === document.querySelector('.header__nav--active')){
+menuBtn.addEventListener('click', () => {
+    if (headerNav === document.querySelector('.header__nav--active')) {
         headerNav.classList.remove('header__nav--active')
         menuBtn.classList.remove('menu__btn--active')
     }
-    else{
+    else {
         headerNav.classList.add('header__nav--active')
         menuBtn.classList.add('menu__btn--active')
     }
 })
-linkItem.forEach(item =>{
-    item.addEventListener('click',()=>{
-        headerNav.classList.remove('header__nav--active')
-        menuBtn.classList.remove('menu__btn--active')
-    })
-})
+
+// linkItem.forEach(item =>{
+//     item.addEventListener('click',()=>{
+//         headerNav.classList.remove('header__nav--active')
+//         menuBtn.classList.remove('menu__btn--active')
+//         let linkId = item.childNodes
+//         console.log("linklinkId" ,linkId)
+
+//         linkId.forEach(link =>{
+//         //    let link.ATTRIBUTE_NODE
+
+//         })
+//     })
+// })
 
 
 
@@ -29,10 +37,56 @@ const headerBtn = document.querySelector('.header__btn-bottom');
 const skillsSection = document.querySelector('.skills');
 
 
-headerBtn.addEventListener('click', () =>{
+headerBtn.addEventListener('click', () => {
     skillsSection.scrollIntoView({
         block: 'nearest', // к ближайшей границе экрана
-        behavior: 'smooth', 
-        
+        behavior: 'smooth',
+
     });
 });
+
+const linkMenu = document.querySelectorAll('.header__menu-link');
+
+linkMenu.forEach(item => {
+   
+    item.addEventListener("click", (e) => {
+        e.preventDefault()
+        linkData = item.getAttribute('href').substring(1)
+        console.log(linkData)
+        let block = document.querySelector(`.${linkData}`)
+        
+        block.scrollIntoView({
+            block: 'end', // к ближайшей границе экрана
+            behavior: 'smooth',
+        });
+    
+    })
+
+
+})
+
+
+
+//fix height VANTA
+const headerScreen = document.querySelector('.header')
+let positionHeight = window.screen.height
+
+headerScreen.style.height = positionHeight + 'px'
+
+
+
+//options VANTA
+VANTA.NET({
+    el: ".header",
+
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: positionHeight,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.0,
+    color: 0x603fff,
+    backgroundColor: 0x0,
+    spacing: 14.00
+})
